84348ec110f13d9486e02d4f70a0815daa1b20f4
proyectosprogramacion <proyectosprogramacionjunior@gmail.com>
2023-07-31 08:19:22 +0200

    estructura

---
+++ src/logicaProgramacionBasicaEjercicios_19_31/OrdenInverso_19.java
@@ -0,0 +1,10 @@
+package logicaProgramacionBasicaEjercicios_19_31;
+
+public class OrdenInverso_19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/logicaProgramacionBasicaEjercicios_1_18/SumarDivisores_1.java
@@ -0,0 +1,10 @@
+package logicaProgramacionBasicaEjercicios_1_18;
+
+public class SumarDivisores_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
